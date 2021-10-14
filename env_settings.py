67a8a7d3eba57c82ecb7b4e97e6eef48c3619ce5
Dzmitry Chatyrka <dzc@activecloud.com>
2021-10-14 13:03:05 +0300

    documentation; small fixes

--- env_settings.py
+++ env_settings.py
@@ -1,5 +1,6 @@
 '''
-add this to settings.py
+add this to the end of settings.py
+env settings for containerization
 
 try:
     from .env_settings import * 
@@ -37,9 +38,9 @@ if DJANGO_ENV == 'development' or DJANGO_ENV == 'production':
     DATABASES = {
         "default": {
             "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
-            "NAME": os.environ.get("DB_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-            "USER": os.environ.get("DB_USER", "user"),
-            "PASSWORD": os.environ.get("DB_PASSWORD", "password"),
+            "NAME": os.environ.get("POSTGRES_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+            "USER": os.environ.get("POSTGRES_USER", "user"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
             "HOST": os.environ.get("DB_HOST", "localhost"),
             "PORT": os.environ.get("DB_PORT", "5432"),
         }
